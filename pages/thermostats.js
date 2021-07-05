2509a324015f3bdd379defc32cd9a8858311a90d
dibyn <dibyn@uxcam.com>
2021-07-05 22:58:22 +0545

    thermostats file

---
+++ pages/thermostats.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import TableWithModal from '../components/TableWithModal'
+const temperatureDisplay = (temperature) => (
+  <span key={temperature}>{temperature} F</span>
+)
+const columns = [
+  {
+    title: 'Thermostats Name',
+    dataIndex: 'thermostat_name',
+    key: 'thermostat_name',
+  },
+  {
+    title: 'Temperature',
+    dataIndex: 'temperature',
+    key: 'temperature',
+    render: temperatureDisplay,
+  },
+]
+const Thermostats = () => (
+  <TableWithModal
+    columns={columns}
+    eventKey={'thermostat'}
+    key={'thermostat'}
+    getKey={'thermostat'}
+  />
+)
+export default Thermostats
